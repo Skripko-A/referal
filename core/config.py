f3b9e6882bf70cd04cda904df7b4b5cbecaeb4fb
a-skripko <skripkoae@gmail.com>
2024-11-13 14:32:51 +0400

    init

---
+++ core/config.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+
+
+class Settings:
+    PROJECT_NAME: str = "Referal"
+    PROJECT_VERSION: str = "0.0.1"
+
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DEBUG = os.getenv("DEBUG", "False") == "True"
+    print(DATABASE_URL)
+
+
+settings = Settings()
