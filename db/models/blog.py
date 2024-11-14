eef7e04a4a7fd5b490d475d1f4b4e33fa07d88cf
a-skripko <skripkoae@gmail.com>
2024-11-14 17:42:24 +0400

    delete comments

--- db/models/blog.py
+++ db/models/blog.py
@@ -13,18 +13,12 @@ from db.base_class import Base
 
 
 class Blog(Base):
-    __tablename__ = "blogs"  # Явно указываем имя таблицы
+    __tablename__ = "blogs"
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
     slug = Column(String, nullable=False)
     content = Column(Text, nullable=False)
-
-    # Используйте правильное имя таблицы для ForeignKey
-    author_id = Column(
-        Integer, ForeignKey("users.id")
-    )  # Исправлено на 'users'
-
-    # Строковая ссылка на User
+    author_id = Column(Integer, ForeignKey("users.id"))
     author = relationship("User", back_populates="blogs")
 
     created_at = Column(DateTime, default=datetime.now())
