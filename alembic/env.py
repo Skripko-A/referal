a6888a2ab7fd60a07256fdc16ca17b97c8470688
a-skripko <skripkoae@gmail.com>
2024-11-15 11:23:02 +0400

    fix db creation

--- alembic/env.py
+++ alembic/env.py
@@ -5,8 +5,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from core.config import settings
-from db.base_class import Base
-
+from db.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,8 +19,8 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-# target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -67,7 +66,9 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
 
         with context.begin_transaction():
             context.run_migrations()
